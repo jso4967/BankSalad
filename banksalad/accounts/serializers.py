77e6dd27b66fdae515283ae1736734d6a3318c04
jso4967 <jso4967@gmail.com>
2019-09-15 14:26:39 +0900

    Pause project

--- banksalad/accounts/serializers.py
+++ banksalad/accounts/serializers.py
@@ -1,19 +1,46 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from rest_framework.response import Response
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+
+def validate_phone_number(value):
+    if not isinstance(value, str):
+        raise ValidationError(
+            _('%(value)s is not string type'),
+            params={'value': value},
+        )
+    value = value.split('-')
+    for numbers in value:
+        if numbers.__len__() == 3 and numbers == "010":
+            pass
+        elif numbers.__len__() == 4:
+            for n in list(numbers):
+                n = int(n)
+                if not 0 <= n and n <= 9:
+                    raise ValidationError(
+                        _('%(value)s is not phone number format'),
+                        params={'value': value},
+                    )
+        else:
+            raise ValidationError(
+                _('%(value)s is not phone number format'),
+                params={'value': value},
+            )
 
 
 # User Serializer
-# 유저(사용자) 시리얼라이저는 간단하기도하고 Delete serializer와도 매우 유사합니다.
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'password')
 
 
-# Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email',)
+        fields = ('username', 'password',)
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_password(self, value):
+        validate_phone_number(value)
--- banksalad/accounts/views.py
+++ banksalad/accounts/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from .serializers import UserSerializer, RegisterSerializer
 
 
-# Register API
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
